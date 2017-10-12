489e963f2294fadc4ea9f022c233cc7fd727113b
Yoni Slotwiner <yonislots@gmail.com>
2017-10-12 18:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function keepGoing(array, changeValue, skipValue) {
   return array
 };
 
-findBy(array, findFn) {
+function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++){
     if(findFn(array[i])){
       return array[i];
